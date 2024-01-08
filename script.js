c7070b5b365bcf604e051e766092297d8783e5c5
Altay Hodoglugil <altay.hodo@gmail.com>
2024-01-07 23:15:34 -0800

    Add checking for password match

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
-const passwordInput = document.querySelector('.password');
+const password = document.querySelector('.password');
 const confirmPassword = document.querySelector('.confirm-password');
+const doNotMatch = document.querySelector('.do-not-match')
+
+confirmPassword.addEventListener('input', checkPasswordMatch);  
+password.addEventListener('input', checkPasswordMatch);
+
+function checkPasswordMatch(){
+  const passwordValue = password.value;
+  const confirmValue = confirmPassword.value;
+  if(passwordValue !== confirmValue && passwordValue && confirmValue){
+    doNotMatch.classList.add('active');
+    password.classList.add('error')
+    confirmPassword.classList.add('error')
+  }else{
+    doNotMatch.classList.remove('active');
+    password.classList.remove('error')
+    confirmPassword.classList.remove('error')
+  }
+  console.log(passwordValue);
+  console.log(confirmValue)
+}
